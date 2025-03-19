a67f7bd82ece0147c92a524cba6f03ba833fddcd
myxz <luoyu2004@gamil.com>
2025-03-19 10:57:37 +0800

    Initial commit

---
+++ resume/.vuepress/config.ts
@@ -0,0 +1,72 @@
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  base: "/",
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "骆裕",
+      description: "骆裕介绍",
+    },
+  },
+
+  theme: hopeTheme({
+    hostname: "https://resume.myxz.fun",
+    favicon: "/favicon.ico",
+
+    author: {
+      name: "Yu Luo",
+      url: "https://resume.myxz.fun",
+    },
+
+    sidebar: false,
+
+    displayFooter: true,
+    editLink: false,
+
+    // blog: {
+    //   medias: {
+    //     Gmail: "mailto:c-shang@iis.u-tokyo.ac.jp",
+    //     Email: "mailto:cheng.shang@riken.jp",
+    //   },
+    // },
+
+    locales: {
+      /**
+       * Chinese locale config
+       */
+      "/": {
+        author: {
+          name: "骆裕",
+          url: "https://resume.myxz.fun",
+        },
+        navbar: [
+          "/",
+          {
+            text: "个人介绍",
+            icon: "circle-info",
+            link: "/#个人信息",
+          },
+        ],
+
+        // blog: {
+        //   description: "博士",
+        //   intro: "/en/#个人信息",
+        // },
+      },
+    },
+
+    markdown: {
+      align: true,
+      hint: false,
+    },
+
+    plugins: {
+      icon: {
+        assets: "fontawesome-with-brands",
+      },
+      photoSwipe: false,
+    },
+  }),
+});
